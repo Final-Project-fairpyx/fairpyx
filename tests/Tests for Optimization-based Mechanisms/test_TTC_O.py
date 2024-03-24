fed1f4e45f09915357eb23d58b9eb2846ffac7bf
MoriyaEster <moriyaeo100@gmail.com>
2024-03-24 23:37:47 +0200

    fix 1 test

--- tests/Tests for Optimization-based Mechanisms/test_TTC_O.py
+++ tests/Tests for Optimization-based Mechanisms/test_TTC_O.py
@@ -78,18 +78,18 @@ def test_optimal_cardinal_utility():
                                                                                   's2': ['c2'],
                                                                                   's3': ['c1']}, "ERROR"
 
-    def test_random():
-        for i in range(NUM_OF_RANDOM_INSTANCES):
-            np.random.seed(i)
-            instance = fairpyx.Instance.random_uniform(
-                num_of_agents=70, num_of_items=10, normalized_sum_of_values=1000,
-                agent_capacity_bounds=[2, 6],
-                item_capacity_bounds=[20, 40],
-                item_base_value_bounds=[1, 1000],
-                item_subjective_ratio_bounds=[0.5, 1.5]
-            )
-            allocation = fairpyx.divide(fairpyx.algorithms.TTC_O_function, instance=instance)
-            fairpyx.validate_allocation(instance, allocation, title=f"Seed {i}, TTC_O_function")
+def test_random():
+    for i in range(NUM_OF_RANDOM_INSTANCES):
+        np.random.seed(i)
+        instance = fairpyx.Instance.random_uniform(
+            num_of_agents=70, num_of_items=10, normalized_sum_of_values=1000,
+            agent_capacity_bounds=[2, 6],
+            item_capacity_bounds=[20, 40],
+            item_base_value_bounds=[1, 1000],
+            item_subjective_ratio_bounds=[0.5, 1.5]
+        )
+        allocation = fairpyx.divide(fairpyx.algorithms.TTC_O_function, instance=instance)
+        fairpyx.validate_allocation(instance, allocation, title=f"Seed {i}, TTC_O_function")
 
 if __name__ == "__main__":
     pytest.main(["-v",__file__])
